9cad52b43eceec0bbcb5fd1e77701e475c7ea2e4
ravi2545 <53468347+ravi2545@users.noreply.github.com>
2019-11-11 10:31:22 +0530

    Update Write a Python program to sum all the items in a list.py

--- Write a Python program to sum all the items in a list.py
+++ Write a Python program to sum all the items in a list.py
@@ -8,10 +8,7 @@ print("Sum of elements in given list is :", sum(lst))
 
 
 #2. Write a Python program to multiplies all the items in a list.
-#3. Write a Python program to get the largest number from a list.
-lis=[]
-n=int(input("Enter Items"))
-for i in range(n):
-    num=int(input("Enter the Nubmer"))
-    lis.append(num)
-print("The sum of the list elements is:", max(lis))
+
+
+
+
