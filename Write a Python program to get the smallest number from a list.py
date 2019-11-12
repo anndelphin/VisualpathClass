c490d96b5e8766e7c5cf39e6a179295bb278d310
ravi2545 <53468347+ravi2545@users.noreply.github.com>
2019-11-12 09:27:30 +0530

    Update Write a Python program to get the smallest number from a list.py

--- Write a Python program to get the smallest number from a list.py
+++ Write a Python program to get the smallest number from a list.py
@@ -1,1 +1,10 @@
+#3.Write a Python program to get the largest number from a list.
 #4.Write a Python program to get the smallest number from a list.
+ls = [1, 2, 3, 0, 9, 7]
+ls.sort()
+print(ls)
+print("The Largest element in the list",ls[-1])
+print("The smallest element in the list",ls[0])
+#Using in builtin Functions
+print("The Largest element in the list",max(ls))
+print("The smallest element in the list",min(ls))
