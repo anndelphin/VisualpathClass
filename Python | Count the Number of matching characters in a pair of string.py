85e18ff5e8d5d130ecb0a409c47ea50f8f8d4216
ravi2545 <53468347+ravi2545@users.noreply.github.com>
2019-11-14 20:36:38 +0530

    Create Python | Count the Number of matching characters in a pair of string.py

---
+++ Python | Count the Number of matching characters in a pair of string.py
@@ -0,0 +1,19 @@
+#Python | Count the Number of matching characters in a pair of string
+str1="RaviP"
+str2="Ra"
+c=0
+j=0
+for i in str1:
+    if str2.find(i)>=0 and j==str1.find(i):
+        c+=1
+    j+=1
+print("Number of character maching is ",c)
+
+
+#Python | Count the Number of matching characters in a pair of string
+str1="RaviP"
+str2="Ra"
+s1=set(str1)
+s2=set(str2)
+m=s1&s2
+print("the Number of matched Elements in the strings are:",str(len(m)))
