dd521a403c7fa003dc5227bfc792e42163d1baf4
ravi2545 <53468347+ravi2545@users.noreply.github.com>
2019-11-14 19:45:48 +0530

    Update Write a Python program to get the smallest number from a list.py

--- Write a Python program to get the smallest number from a list.py
+++ Write a Python program to get the smallest number from a list.py
@@ -1,10 +1,23 @@
-#3.Write a Python program to get the largest number from a list.
-#4.Write a Python program to get the smallest number from a list.
-ls = [1, 2, 3, 0, 9, 7]
-ls.sort()
-print(ls)
-print("The Largest element in the list",ls[-1])
-print("The smallest element in the list",ls[0])
-#Using in builtin Functions
-print("The Largest element in the list",max(ls))
-print("The smallest element in the list",min(ls))
+#3. Write a Python program to get the largest number from a list.
+lis=[]
+n=int(input("Enter Items"))
+for i in range(n):
+    num=int(input("Enter the Nubmer"))
+    lis.append(num)
+print("The sum of the list elements is:", min(lis))
+
+
+#Write a Python program to multiplies all the items in a list.
+NumList=[2,1,4,5,5,5,6,7,7] #The Length of the list consider as starting 1 to n
+largest = NumList[0]    
+for j in range(1, len(NumList)):
+    if(largest > NumList[j]):
+        largest = NumList[j]
+
+print("The Smallest Element in this List is : ", largest)
+
+
+#using sorting Method
+k=[1,2,3,4,4,5]
+k.sort()
+print(k[0])
