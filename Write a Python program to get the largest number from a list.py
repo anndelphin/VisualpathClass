1c24bf644f7ce894b2654c43939d4833e604d731
ravi2545 <53468347+ravi2545@users.noreply.github.com>
2019-11-14 19:41:56 +0530

    Update Write a Python program to get the largest number from a list.py

--- Write a Python program to get the largest number from a list.py
+++ Write a Python program to get the largest number from a list.py
@@ -7,5 +7,20 @@ for i in range(n):
 print("The sum of the list elements is:", max(lis))
 
 
+#Write a Python program to multiplies all the items in a list.
+NumList=[2,1,4,5,5,5,6,7,7] #The Length of the list consider as starting 1 to n
+largest = NumList[0]    
+for j in range(1, len(NumList)):
+    if(largest < NumList[j]):
+        largest = NumList[j]
+
+print("The Largest Element in this List is : ", largest)
+
+
+#using sorting Method
+k=[1,2,3,4,4,5]
+k.sort()
+print(k[-1])
+
 
 
