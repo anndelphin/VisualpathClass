867712873fe1a1a022ee7c5394aa22fbcf9bea02
ravi2545 <53468347+ravi2545@users.noreply.github.com>
2019-11-25 17:56:08 +0530

    Update and rename Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.py to #12.Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.py

--- Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.
-
-
-k=[1,23,3,3,5,3,4]
-k=[x for (i,x) in enumerate(k) if i not in (0,4,5)]
-print(k)
